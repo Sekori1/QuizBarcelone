334e556b6097d66d62c1745d9c5479aab60cf441
Sekori <romaric55220@gmail.com>
2019-03-12 23:08:56 +0100

    changes

--- app/src/main/java/romaricgauzi/fr/quizmadrid/NewGameActivity.java
+++ app/src/main/java/romaricgauzi/fr/quizmadrid/NewGameActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -19,6 +20,10 @@ public class NewGameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.new_game);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setSubtitle("Using ToolBar");
 
         this.editText = findViewById(R.id.teamName);
     }
